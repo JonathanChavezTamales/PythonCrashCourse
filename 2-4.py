c776f18736f442fc76473ea1522ae6eab44dd3ae
Jonathan Chavez <jonathanchatab@hotmail.com>
2018-08-19 00:08:18 -0500

    Agregado hasta 2-4 y corrección error correo commits

---
+++ 2-4.py
@@ -0,0 +1,7 @@
+'''
+Store a person’s name in a variable, and then print that per- son’s name
+in lowercase, uppercase, and titlecase .
+'''
+
+name = "Jonathan"
+print(name.upper() + " " + name + " " + name.lower() + " " + name.title())
