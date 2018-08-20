f77b5e18443340aa851e7249e85d26cc47102cb3
Jonathan Chavez <jonathanchatab@hotmail.com>
2018-08-20 13:42:07 -0500

    Restantes agregados, falta código, pero indices al corriente

---
+++ 5-8.py
@@ -0,0 +1,6 @@
+'''
+5-8. Hello Admin: Make a list of five or more usernames, including the name
+'admin' . Imagine you are writing code that will print a greeting to each user
+ after they log in to a website . Loop through the list, and print a greeting
+  to each user:
+'''
---
+++ 5-9.py
@@ -0,0 +1,4 @@
+'''
+5-9. No Users: Add an if test to hello_admin.py to make sure the list of
+users is not empty .
+'''
