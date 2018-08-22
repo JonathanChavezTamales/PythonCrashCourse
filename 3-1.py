dbdfb2f6942e633535f335d03255e37fc4886cfc
Jonathan Chavez <jonathanchatab@hotmail.com>
2018-08-22 07:30:17 -0500

    Agregando 3-1

--- 3-1.py
+++ 3-1.py
@@ -2,3 +2,7 @@
 3-1. Names: Store the names of a few of your friends in a list called names.
 Print each person’s name by accessing each element in the list, one at a time .
 '''
+names = ["Juan", "Pedro", "Ramiro", "Juan2"]
+
+for name in names:
+    print(name)
